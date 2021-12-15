d69d8e2d47ff5d907a3ffe47d7e636bd7a3c0f23
a279623002 <970979353@qq.com>
2021-12-15 09:40:16 +0800

    add

--- leetcode/51to75/project70/main.go
+++ leetcode/51to75/project70/main.go
@@ -9,7 +9,21 @@ import "fmt"
 //2.  1 阶 + 2 阶
 //3.  2 阶 + 1 阶
 func climbStairs(n int) int {
-	//fuck
+	// 0 =》 1
+	// 1 =》 1
+	// 2 =》 2
+	// 3 =》 3
+	// 4 =》 5
+	// 5 =》 8
+	// dp|i| = dp|i-1| + dp|i-2|
+	o, t, res := 0, 0, 1
+	for i := 0; i < n; i++ {
+		// 滚动数组
+		o = t
+		t = res
+		res = o + t
+	}
+	return res
 }
 
 func main() {
