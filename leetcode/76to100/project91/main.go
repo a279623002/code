8eec012d2b483a1acbce6cd2f4cad789b79f610c
a279623002 <970979353@qq.com>
2022-01-07 14:10:57 +0800

    add

--- leetcode/76to100/project91/main.go
+++ leetcode/76to100/project91/main.go
@@ -9,16 +9,17 @@ import "fmt"
 //输入：s = "12"
 //输出：2
 //解释：它可以解码为 "AB"（1 2）或者 "L"（12）。
+
 func numDecodings(s string) int {
-	// dp|i| = dp|i-1| + dp|i-2| (nums[i-2] != 0 && nums[i-1] != 0 && nums[i-2:i]<27)
+	// dp[i] = dp[i-1] (s[i-1] > 0) + dp[i-2] (i>1 && s[i-2] > 0 && s[i-2]*10+s[i-1] < 27)
 	n := len(s)
 	dp := make([]int, n+1)
 	dp[0] = 1
 	for i:=1; i<=n; i++ {
-		if s[i-1]!= '0' {
+		if s[i-1] != '0' {
 			dp[i] += dp[i-1]
 		}
-		if i > 1 && s[i-2] != '0' && (s[i-2]-'0')*10+(s[i-1]-'0') < 27 {
+		if i > 1 && s[i-2] != '0' && (s[i-2]-'0')*10+s[i-1]-'0' < 27 {
 			dp[i] += dp[i-2]
 		}
 	}
@@ -30,5 +31,5 @@ func main() {
 	// 12  3 4 5
 	// 1 23  4 5
 	//
-	fmt.Println(numDecodings("3123"))
+	fmt.Println(numDecodings("226"))
 }
