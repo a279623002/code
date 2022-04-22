ff571db97c8db4c6a0ec43c46f950bc5a7d7d570
a279623002 <970979353@qq.com>
2022-04-22 17:54:46 +0800

    del

--- MTBSystem/src/film-srv/entity/filmActor.go
+++
@@ -1,8 +0,0 @@
-package entity
-
-type FilmActor struct {
-	FilmId    int64  `json:"film_id" db:"film_id"`
-	FilmName  string `json:"film_name" db:"film_name"`
-	ActorId   int64  `json:"actor_id" db:"actor_id"`
-	ActorName string `json:"actor_name" db:"actor_name"`
-}
