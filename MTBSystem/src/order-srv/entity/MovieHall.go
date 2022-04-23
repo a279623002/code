8b2147a31e290d46c5cac802dd61cc287a360be7
a279623002 <970979353@qq.com>
2022-04-23 18:58:36 +0800

    add

--- MTBSystem/src/order-srv/entity/MovieHall.go
+++
@@ -1,8 +0,0 @@
-package entity
-
-type MovieHall struct {
-	MhId     int64   `json:"mh_id" db:"mh_id"`
-	MhName    string  `json:"mh_name" db:"mh_name"`
-	MhAddress int64   `json:"mh_address" db:"mh_address"`
-	CinemaId  int64   `json:"cinema_id" db:"cinema_id"`
-}
--- demo/example/mynet.go
+++
@@ -1,7 +0,0 @@
-package example
-
-import "net/http"
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello world"))
-}
