dfc8494e8647faf66482e40693d8a51c865c4264
shiro <970979353@qq.com>
2023-01-09 12:00:02 +0800

    [新增功能](master): add

---
+++ note/goMode/flyweightPattern/flyweightPattern.go
@@ -0,0 +1,36 @@
+package flyweightPattern
+
+// 共享的享元类型
+type Flyweight struct {
+	Data string
+}
+
+// 资源工厂
+type FlyweightFactory struct {
+	memory map[string]*Flyweight
+}
+
+func (f *FlyweightFactory) GetFlyweight(name string) *Flyweight {
+	flyweight, ok := f.memory[name]
+	if !ok {
+		flyweight = &Flyweight{Data: name}
+		f.memory[name] = flyweight
+	}
+	return flyweight
+}
+
+var factory *FlyweightFactory
+
+func GetFactory() *FlyweightFactory {
+	if factory == nil {
+		factory = &FlyweightFactory{memory: make(map[string]*Flyweight)}
+	}
+	return factory
+}
+
+// 具体的享元类型
+func New(name string) string {
+	flyweightFactory := GetFactory()
+	flyweight := flyweightFactory.GetFlyweight(name)
+	return flyweight.Data
+}
---
+++ note/goMode/flyweightPattern/flyweightPattern_test.go
@@ -0,0 +1,11 @@
+package flyweightPattern
+
+import (
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	if res := New("zzq"); res != "zzq" {
+		t.Errorf("res expected be zzq, but %s got", res)
+	}
+}
