0d24ccd61c477ce50353d8bae69cf462dcc143ed
a279623002 <970979353@qq.com>
2024-10-10 19:52:25 +0800

    add

--- assets/js/tab.js
+++ assets/js/tab.js
@@ -24,4 +24,23 @@ $(function () {
             }, 'fast'); // 'slow'定义动画执行的速度
         }
     })
+
+    $(".back").attr("href", "javascript:back()");
 })
+
+function back() {
+    // 获取当前页面的完整URL  
+    var currentUrl = window.location.href;
+
+    currentUrl = currentUrl.split('/').slice(0, -1).join('/');
+
+    var newPath = currentUrl
+    // 如果新的路径为空字符串（即当前URL是根路径），则新路径应设为'/'  
+    if (newPath === '') {
+        newPath= '/';
+    }
+
+    window.location.href = newPath;
+
+}
+
