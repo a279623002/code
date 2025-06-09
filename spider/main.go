6136e66b80b63fed4815f2f4bc642d7d04ae10b9
a279623002 <970979353@qq.com>
2025-06-09 10:30:28 +0800

    add

--- spider/main.go
+++ spider/main.go
@@ -1,14 +1,16 @@
 package main
 
-import (
-	"spider/platform"
-	"sync"
-)
-
 func main() {
-	p := platform.NewTaobao("./download/taobao", &sync.WaitGroup{})
-	// p := platform.NewJingdong("./download/jingdong", &sync.WaitGroup{})
+	// p := platform.NewTaobao("./download/taobao", &sync.WaitGroup{})
+	// // p := platform.NewJingdong("./download/jingdong", &sync.WaitGroup{})
 
-	pm := platform.NewPlatformManager(p)
-	pm.Start()
+	// pm := platform.NewPlatformManager(p)
+	// pm.Start()
+	s := []int{1, 2, 3, 4, 5}
+	for k, _ := range s {
+		s[k] = 0
+	}
+	for _, v := range s {
+		print(v)
+	}
 }
